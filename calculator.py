849488cadbf04bbeafd2b8a9176748fb0ef4c2a3
YashikaAnand181 <yashikaanand17121@gmail.com>
2023-07-10 21:40:28 +0530

    added calculator file

---
+++ calculator.py
@@ -0,0 +1,104 @@
+a=input("Enter the first number here: ")
+b=input("Enter the second number here: ")
+print("Operators are +, -, *, /, % .......")
+choice=input("Choose any operator from above: ")
+if(choice=="+"):
+    print("The answer is ",int(a)+int(b))
+if (choice == "-"):
+    print("The answer is ",int(a)-int(b))
+if(choice=="*"):
+    print("The answer is ",int(a)*int(b))
+if(choice=="/"):
+    print("The answer is ",int(a)/int(b))
+if(choice=="%"):
+    print("The answer is ",int(a)%int(b))
+
+print("\nSuccessfully made a very simple calculator :) ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
