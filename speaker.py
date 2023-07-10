ca3270a704ff6051b6cbfe43830a304d77faad81
YashikaAnand181 <138115596+YashikaAnand181@users.noreply.github.com>
2023-07-10 22:11:38 +0530

    Delete speaker.py

--- speaker.py
+++
@@ -1,6 +0,0 @@
-#------This speaker speaks for you-----
-import pyttsx3
-speak= pyttsx3.init()
-speak.say("Hello Friends! WhatsUp?")
-speak.say("Welcome to PyCharm. Start Coding with a :) ")
-speak.runAndWait()
